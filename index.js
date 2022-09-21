adb867d82f205948cc5e6f714ca1adb29f8057ed
Naimul Islam <ms0413354@gmail.com>
2022-09-21 23:11:41 +0600

    Mongoose Successfully Connected.

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const Port = process.env.PORT || 5000;
 const cors = require('cors');
 const colors = require('colors');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+// Connect Mongoose
+mongoose
+	.connect(process.env.MONGOOSE)
+	.then(() => console.log('Mongoose Successfully Connected!'.yellow.bold))
+	.catch((err) => console.log(err));
 
 // import route
 const hellowRoute = require('./Route/v1/hellow.route');
@@ -16,7 +23,5 @@ app.use(express.json());
 app.use('/', hellowRoute);
 
 app.listen(Port, () => {
-	console.log(
-		`Tour Management System server is running: ${Port}`.bgRed.white.bold
-	);
+	console.log(`Tour Management System server is running: ${Port}`.red.bold);
 });
